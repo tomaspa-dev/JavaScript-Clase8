74574c939621ef67a7bfe6fec7ed50e00212ff09
tomaspa-dev <blord.dev@gmail.com>
2023-09-10 11:36:13 -0500

    arrow Functions

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ calcAge(1991); */
 // matilda.calcAge();
 
 // const f = jonas.calcAge;
+var firstName = 'Matilda';
 
 const jonas = {
   firstName: 'Jonas',
@@ -119,9 +120,41 @@ const jonas = {
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
+
+    // //Solution1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    //Solution2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
 
+  //Never use arrow function like a method
   greet: () => console.log(`Hey ${this.firstName}`),
 };
 
 jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow(2, 5, 8);
